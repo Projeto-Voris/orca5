b37489c10e249f609ee5324b9e94e9ad6adac66a
Subimu <biancamullerosa@gmail.com>
2026-05-22 16:17:08 -0300

    feat: implement basic RC override waypoint controller

--- voris_base/include/rc_control.hpp
+++ voris_base/include/rc_control.hpp
@@ -3,13 +3,19 @@
 
 #include <memory>
 #include <string>
+#include <vector>
+#include <cmath>
 
 #include <rclcpp/rclcpp.hpp>
 #include "mavros_msgs/msg/override_rc_in.hpp"
 #include "mavros_msgs/srv/command_bool.hpp"
 #include "mavros_msgs/srv/set_mode.hpp"
-#include "nav2_util/service_client.hpp"
+#include "mavros_msgs/msg/state.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/utils.h"
 
 // construtor da classe
 class RCControl : public rclcpp::Node
@@ -18,16 +24,51 @@ public:
     RCControl();
 
 private:
+    bool connected_{};
+    bool armed_{};
+    //string está dentro de std
+    std::string mode_ = "";
+
     // Subscribers 
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr odometry_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    void odom_cb(const nav_msgs::msg::Odometry::ConstSharedPtr & msg);
+
+    rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_sub_;
+    void state_cb(const mavros_msgs::msg::State::ConstSharedPtr & msg);
+
+    // Current robot state
+    geometry_msgs::msg::PoseStamped current_pose_;
+    geometry_msgs::msg::Twist current_vel_;
 
     // Publishers
     rclcpp::Publisher<mavros_msgs::msg::OverrideRCIn>::SharedPtr rc_pub_;
 
     // Services
-    std::shared_ptr<nav2_util::ServiceClient<mavros_msgs::srv::CommandBool>> mavros_arm_client_;
-    std::shared_ptr<nav2_util::ServiceClient<mavros_msgs::srv::SetMode>> mavros_set_mode_client_;   
+    rclcpp::Client<mavros_msgs::srv::CommandBool>::SharedPtr mavros_arm_client_;
+    rclcpp::Client<mavros_msgs::srv::SetMode>::SharedPtr mavros_set_mode_client_;   
 
     // timer
     rclcpp::TimerBase::SharedPtr timer_;
-}
+
+    // functions
+    bool set_arm(bool arm);
+    bool disarm();
+    bool set_mode(const std::string & mode);
+    uint16_t map_pwm(float value, float threshold);
+    void publish_rc(float forward, float lateral, float yaw);
+    void follow_wp();
+
+    struct waypoint
+    {
+        double x;
+        double y;
+        double z;
+    };
+
+    // Definitions
+    std::vector<waypoint> wp_;
+    double gain_;
+    size_t index_wp_;
+};
+
+#endif // RC_CONTROL_HPP
--- voris_base/src/rc_control.cpp
+++ voris_base/src/rc_control.cpp
@@ -6,8 +6,12 @@ RCControl::RCControl() : Node("rc_control")
     rc_pub_ = this->create_publisher<mavros_msgs::msg::OverrideRCIn>("mavros/rc/override", 10);
 
     // Initialize services
-    mavros_arm_client_ = std::make_shared<nav2_util::ServiceClient<mavros_msgs::srv::CommandBool>>(this, "mavros/cmd/arming");
-    mavros_set_mode_client_ = std::make_shared<nav2_util::ServiceClient<mavros_msgs::srv::SetMode>>(this, "mavros/set_mode");
+    mavros_arm_client_ = this->create_client<mavros_msgs::srv::CommandBool>("mavros/cmd/arming");
+    mavros_set_mode_client_ = this->create_client<mavros_msgs::srv::SetMode>("/mavros/set_mode");
+
+    // Subcriptions
+    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/model/bluerov2/odometry", 10, std::bind(&RCControl::odom_cb, this, std::placeholders::_1));
+    state_sub_ = this->create_subscription<mavros_msgs::msg::State>("/mavros/state", 10, std::bind(&RCControl::state_cb, this, std::placeholders::_1));
 
     // Initialize timer to publish RC commands at a fixed rate
     timer_ = this->create_wall_timer(
@@ -19,7 +23,26 @@ RCControl::RCControl() : Node("rc_control")
 
     gain_ = 0.5;
 
-    wp = {0.0, 0.0, -1.0}; // Example waypoint, replace with actual logic to set waypoints
+    index_wp_ = 0;
+    wp_ = {{4.0, 0.0, 0.0}, {4.0, -2.5, 0.0}, {0.0, -2.5, 0.0}, {0.0, 0.0, 0.0}};
+}
+
+void RCControl::state_cb(const mavros_msgs::msg::State::ConstSharedPtr & msg)
+{
+    connected_ = msg->connected;
+    armed_ = msg->armed;
+    mode_ = msg->mode;
+}
+
+void RCControl::odom_cb(const nav_msgs::msg::Odometry::ConstSharedPtr & msg)
+{
+    // position (position and orientation)
+    current_pose_.header = msg->header;
+    current_pose_.pose = msg->pose.pose;
+    RCLCPP_INFO(this->get_logger(), "x:%4f | y:%4f", current_pose_.pose.position.x, current_pose_.pose.position.y);
+    
+    // velocity (linear and angular)
+    current_vel_ = msg->twist.twist;
 }
 
 bool RCControl::set_arm(bool arm)
@@ -27,43 +50,40 @@ bool RCControl::set_arm(bool arm)
     auto request = std::make_shared<mavros_msgs::srv::CommandBool::Request>();
     request->value = arm;
     // call mavros/cmd/arming
-    auto response = std::make_shared<mavros_msgs::srv::CommandBool::Response>();
-    RCLCPP_INFO(get_logger(), arm ? "arming..." : "disarming...");
-    auto result = mavros_arm_client_->invoke(request, response);
-    result = result && response->success;
-    RCLCPP_INFO(get_logger(), result ? "success" : "failure");
-    return result;
+    if(mavros_arm_client_->service_is_ready()) {
+        mavros_arm_client_->async_send_request(request);
+        RCLCPP_INFO(this->get_logger(), "Robot Armed");
+        return true;
+    }
+    return false;
+}
+
+bool RCControl::disarm()
+{
+    if (!connected_) {
+        return false; 
+    }
+    if (armed_) {
+        bool result = set_arm(false);
+        if (result) {
+        RCLCPP_INFO(get_logger(), "Robot disarmed");
+        }
+        return result;
+    }
+
+    return true;
 }
 
 bool RCControl::set_mode(const std::string & mode)
 {
     auto request = std::make_shared<mavros_msgs::srv::SetMode::Request>();
     request->custom_mode = mode;
-    // change mode of the ardusub calling /mavros/set_mode
-    auto response = std::make_shared<mavros_msgs::srv::SetMode::Response>();
-    RCLCPP_INFO(get_logger(), "setting mode to %s...", mode.c_str());
-    auto result = mavros_set_mode_client_->invoke(request, response);
-    result = result && response->mode_sent;
-    RCLCPP_INFO(get_logger(), result ? "success" : "failure");
-    return result;
-}
-
-void RCControl::publish_rc(mavros_msgs::msg::OverrideRCIn & msg)
-{
-    for (uint16_t & channel : msg.channels) {
-        channel = mavros_msgs::msg::OverrideRCIn::CHAN_NOCHANGE;
-      }
-
-    msg.channels[5 - 1] = map_pwm(msg, 0.05);
-    msg.channels[6 - 1] = map_pwm(msg, 0.05);
-    msg.channels[4 - 1] = map_pwm(msg, 0.05);
-
-    rc_pub_->publish(msg);
-}
-
-void RCControl::follow_wp()
-{
-    
+    if(mavros_set_mode_client_->service_is_ready()) {
+        mavros_set_mode_client_->async_send_request(request);
+        RCLCPP_INFO(this->get_logger(), "Robot change mode to: ");
+        return true;
+    }
+    return false;
 }
 
 uint16_t RCControl::map_pwm(float value, float threshold)
@@ -73,4 +93,88 @@ uint16_t RCControl::map_pwm(float value, float threshold)
     }
     int pwm = 1500 + (value * 400.0 * gain_);
     return static_cast<uint16_t>(std::clamp(pwm, 1100, 1900));
+}
+
+void RCControl::publish_rc(float forward, float lateral, float depth)
+{
+    mavros_msgs::msg::OverrideRCIn rc_msg;
+
+    for (uint16_t & channel : rc_msg.channels) {
+        channel = mavros_msgs::msg::OverrideRCIn::CHAN_NOCHANGE;
+      }
+    
+    // rc_msg.channels[0] = 1500
+    // rc_msg.channels[1] = 1500
+    // rc_msg
+    rc_msg.channels[5 - 1] = map_pwm(forward, 0.05); // Forward 
+    rc_msg.channels[6 - 1] = map_pwm(lateral, 0.05); // Lateral 
+    rc_msg.channels[3 - 1] = map_pwm(depth, 0.05); // Depth
+
+    RCLCPP_INFO(this->get_logger(), "MOV: Frwd:%4d | Side:%4d | depth:%4d", rc_msg.channels[5 - 1], rc_msg.channels[6 - 1], rc_msg.channels[3 - 1]);
+
+    rc_pub_->publish(rc_msg);
+}
+
+void RCControl::follow_wp()
+{
+    if (index_wp_ >= wp_.size()) {
+        RCLCPP_INFO(get_logger(), "All waypoints reached");
+        disarm();
+        publish_rc(0.0, 0.0, 0.0);
+        return;
+    } else {
+        RCLCPP_INFO_ONCE(get_logger(), "Starting follow waypoints");
+        if (!connected_) {
+            RCLCPP_INFO(get_logger(), "Robot not connected");
+            return;
+        }
+        if (mode_!= "MANUAL") {
+            set_mode("MANUAL");
+            return;
+        }
+        if (!armed_) {
+            set_arm(true);
+            return;
+        }
+    }
+
+    auto target_wp_ = wp_[index_wp_];
+
+    // erro_pose_ = wp - current_pose_
+    double error_x = target_wp_.x - current_pose_.pose.position.x;
+    // coloquei soma aqui porque a direção do robo difere com a coordenada do robo, talvez uma transformação seria mais ideal
+    double error_y = target_wp_.y + current_pose_.pose.position.y;
+    double error_z = target_wp_.z - current_pose_.pose.position.z;
+    // double target_yaw = std::atan2(error_y, error_x);
+    // double error_yaw = target_yaw - current_pose_.pose.orientation.z;
+    RCLCPP_INFO(this->get_logger(), "error_x:%4f | error_y:%4f | error_z:%4f", error_x, error_y, error_z);
+    // erro_vel_ = wp_vel_ - current_vel_ // pensar na logica depois talvez definir uma porcentagem 
+
+    // trasformar para as coordenadas do corpo
+    // double yaw = tf2::getYaw(current_pose_.pose.orientation);
+    // double body_x = std::cos(yaw)*error_x+std::sin(yaw)*error_y;
+    // double body_y = -std::sin(yaw)*error_x+std::cos(yaw)*error_y;
+    // RCLCPP_INFO(this->get_logger(), "body_x:%4f | body_y:%4f", body_x, body_y);
+
+    // definir um ganho para definir como o robo reage conforme a distancia, londe mais rapido perto mais devagar (segurança do movimento)
+    publish_rc(error_x, error_y, error_z);
+
+    // threshold para quando se aproximar suficiente do ponto trocar para o proximo
+    float threshold = 0.5;
+    double distance = std::sqrt(error_x*error_x + error_y*error_y + error_z*error_z);
+    if (distance < threshold)
+    {
+        RCLCPP_INFO(get_logger(), "waypoint %ld reached", index_wp_);
+        index_wp_++;
+        return;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RCControl>(); 
+    rclcpp::spin(node); 
+    rclcpp::shutdown();
+    return 0;
 }
